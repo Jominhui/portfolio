5cbaca30252f98e611257db5a82794ffb844e69a
Jominhui <rihito2021@naver.com>
2021-03-24 10:58:59 +0900

    ADD: models

---
+++ src/models/projects.ts
@@ -0,0 +1,23 @@
+import memory from "../assets/images/Memory/memory.svg";
+import Ida from "../assets/images/Ida/IDA.svg";
+
+const Project = [
+  {
+    name: "memory.log",
+    info:
+      "롤링페이퍼를 웹 사이트에서 제작하게 하여 롤링 페이퍼를 잃어버리지 않고 즐길 수 있도록 제작하였습니다. 대구 ICT 융합 엑스포에서 전시, 발표를 하였습니다.",
+    date: "2020년 08월 | 2020년 12월",
+    url: "/project/memory.log",
+    image: memory,
+  },
+  {
+    name: "IDa",
+    info:
+      "대구소프트웨어고등학교 신입생 입학 시스템입니다. 학생들이 쉽고 편리하게 원서 접수를 할 수 있도록 제작 중입니다. 프론트엔드는 React, MobX, Scss, TypeScript로 제작 중이며, 백엔드은 SpringBoot, Kotlin, Swagger, Thymeleaf 로 제작 중입니다.",
+    date: "2020년 08월 | 진행중",
+    url: "/project/ida",
+    image: Ida,
+  },
+];
+
+export default Project;
---
+++ src/models/skills.ts
@@ -0,0 +1,44 @@
+import react from "../assets/images/Icon/React.svg";
+import js from "../assets/images/Icon/js.svg";
+import ts from "../assets/images/Icon/typescript.svg";
+import html from "../assets/images/Icon/html.svg";
+import css from "../assets/images/Icon/css.svg";
+
+const Skills = [
+  {
+    image: react,
+    className: "react",
+    check: "87%",
+    color: "#61dafb",
+    animation: "react",
+  },
+  {
+    image: js,
+    className: "js",
+    check: "78%",
+    color: "#f7df1e",
+    animation: "js",
+  },
+  {
+    image: ts,
+    className: "ts",
+    check: "70%",
+    color: "#3178c6",
+    animation: "ts",
+  },
+  {
+    image: html,
+    className: "html",
+    check: "95%",
+    color: "#e44d26",
+    animation: "html",
+  },
+  {
+    image: css,
+    className: "css",
+    check: "95%",
+    color: "#0170ba",
+    animation: "css",
+  },
+];
+export default Skills;
