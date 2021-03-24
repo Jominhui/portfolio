dba22ef38dcac7afc88f8ca8b7f7f1821dea9725
Jominhui <rihito2021@naver.com>
2021-03-24 11:50:32 +0900

    test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -19,10 +19,26 @@ const App = () => {
           path="https://jominhui.github.io/portfolio/"
           component={MainPage}
         />
-        <Route exact path="/profile" component={profilePage} />
-        <Route exact path="/project" component={projectPage} />
-        <Route exact path="/project/memory.log" component={projectInfoPage} />
-        <Route exact path="/project/ida" component={projectInfoPage} />
+        <Route
+          exact
+          path="https://jominhui.github.io/portfolio/profile"
+          component={profilePage}
+        />
+        <Route
+          exact
+          path="https://jominhui.github.io/portfolio/project"
+          component={projectPage}
+        />
+        <Route
+          exact
+          path="https://jominhui.github.io/portfolio/project/memory.log"
+          component={projectInfoPage}
+        />
+        <Route
+          exact
+          path="https://jominhui.github.io/portfolio/project/ida"
+          component={projectInfoPage}
+        />
       </Switch>
     </div>
   );
