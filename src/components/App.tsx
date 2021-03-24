2fa52778bfb01faa14584af0341f348da1edfad7
Jominhui <rihito2021@naver.com>
2021-03-24 10:56:09 +0900

    UPDATE: Route

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import profilePage from "../pages/ProfilePage";
+import projectPage from "../pages/ProjectPage";
+import projectInfoPage from "../pages/ProjectInfoPage";
 
 const App = () => {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={MainPage} />
+        <Route exact path="/profile" component={profilePage} />
+        <Route exact path="/project" component={projectPage} />
+        <Route exact path="/project/memory.log" component={projectInfoPage} />
+        <Route exact path="/project/ida" component={projectInfoPage} />
       </Switch>
     </div>
   );
