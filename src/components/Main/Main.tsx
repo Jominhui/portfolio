4574fb7b90f0bf06bdf9d058a191c20a42112be4
Jominhui <rihito2021@naver.com>
2021-03-24 10:57:10 +0900

    UPDATE: Change color & animation

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Main.scss";
 
 interface MainProps {}
@@ -14,7 +15,7 @@ const Main = ({}: MainProps) => {
           <div className="main-content-title">
             <p className="main-content-title-text2">Way To</p>
             <p className="main-content-title-text3"> Develop</p>
-            <div className="main-content-title-under"></div>
+            <div className="main-content-title-under" />
           </div>
           <div className="main-content-info">
             소소하게 살아가는, 보고싶은 것을
@@ -22,8 +23,20 @@ const Main = ({}: MainProps) => {
             하고싶은 것을 하는 개발자
           </div>
           <div className="main-content-btnarea">
-            <div className="main-content-btnarea-btn">프로젝트</div>
-            <div className="main-content-btnarea-btn">프로필</div>
+            <Link to="/project" className="main-content-btnarea-btn">
+              Portfolio
+              <i className="line1"></i>
+              <i className="line2"></i>
+              <i className="line3"></i>
+              <i className="line4"></i>
+            </Link>
+            <Link to="/profile" className="main-content-btnarea-btn">
+              Profile
+              <i className="line1"></i>
+              <i className="line2"></i>
+              <i className="line3"></i>
+              <i className="line4"></i>
+            </Link>
           </div>
         </div>
       </div>
